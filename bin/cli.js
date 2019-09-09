1aabc8d9f3e876ccc0124fb413050f51c414f2ee
Miki <amoo_miki@yahoo.com>
2019-09-08 22:16:54 -0700

    Add name to cli

--- bin/cli.js
+++ bin/cli.js
@@ -30,6 +30,7 @@ const proxy = Proxy();
 const emitter = new EventEmitter();
 
 program
+    .name('tuya-lan-find')
     .option('--ip <ip>', 'IP address to listen for requests')
     .option('-p, --port <port>', 'port the proxy should listen on', 8080)
     .option('--schema', 'include schema in the output')
